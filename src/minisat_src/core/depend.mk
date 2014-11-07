69fa8438121c004cc3ecf83b2b8aa214705c8361
zkaiwen <zkaiwen@gmail.com>
2014-11-06 21:15:47 -0500

    Updated src

---
+++ src/minisat_src/core/depend.mk
@@ -0,0 +1,27 @@
+/local/kaiwen/smflow/external_source/minisat_src/core/Main.o /local/kaiwen/smflow/external_source/minisat_src/core/Main.or /local/kaiwen/smflow/external_source/minisat_src/core/Main.od /local/kaiwen/smflow/external_source/minisat_src/core/Main.op: /local/kaiwen/smflow/external_source/minisat_src/core/Main.cc \
+ /local/kaiwen/smflow/external_source/minisat_src/utils/System.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Int-Types.h \
+ /local/kaiwen/smflow/external_source/minisat_src/utils/ParseUtils.h \
+ /local/kaiwen/smflow/external_source/minisat_src/utils/Options.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Vec.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/XAlloc.h \
+ /local/kaiwen/smflow/external_source/minisat_src/core/Dimacs.h \
+ /local/kaiwen/smflow/external_source/minisat_src/core/SolverTypes.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Alg.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Map.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Alloc.h \
+ /local/kaiwen/smflow/external_source/minisat_src/core/Solver.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Heap.h
+/local/kaiwen/smflow/external_source/minisat_src/core/Solver.o /local/kaiwen/smflow/external_source/minisat_src/core/Solver.or /local/kaiwen/smflow/external_source/minisat_src/core/Solver.od /local/kaiwen/smflow/external_source/minisat_src/core/Solver.op: /local/kaiwen/smflow/external_source/minisat_src/core/Solver.cc \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Sort.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Vec.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Int-Types.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/XAlloc.h \
+ /local/kaiwen/smflow/external_source/minisat_src/core/Solver.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Heap.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Alg.h \
+ /local/kaiwen/smflow/external_source/minisat_src/utils/Options.h \
+ /local/kaiwen/smflow/external_source/minisat_src/utils/ParseUtils.h \
+ /local/kaiwen/smflow/external_source/minisat_src/core/SolverTypes.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Map.h \
+ /local/kaiwen/smflow/external_source/minisat_src/mtl/Alloc.h
